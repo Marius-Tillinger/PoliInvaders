ac6a0841af2e4a04aeda32c99e00ea2d98a1eddd
Marius Petru Tillinger <marius.tillinger@gmail.com>
2023-03-17 14:02:09 +0200

    we have shooting

--- Assets/Script/Player_Movement.cs
+++ Assets/Script/Player_Movement.cs
@@ -1,7 +1,9 @@
+using UnityEditor.Search;
 using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    public Projectile laserPrefab;
     private void Start () {
         // Set the cursor to not be visible
         Cursor.visible = false;
@@ -9,12 +11,19 @@ public class Player_Movement : MonoBehaviour
 
     private void Update() {
         // Get the mouse position and move the player to that position
-
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         mousePosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
 
         transform.position = mousePosition;
+        
+        // Shoot when the left mouse button is pressed
+        if (Input.GetMouseButtonDown(0)) {
+            Shoot();
+        }
+    }
 
+    private void Shoot() {
+        Instantiate(this.laserPrefab, this.transform.position, Quaternion.identity);
     }
 }
